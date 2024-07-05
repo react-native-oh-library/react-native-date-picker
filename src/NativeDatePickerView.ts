950f661244cd9c63d5bbf3a067491d45fa790645
dingyuanjie <911260800@qq.com>
2024-07-05 11:46:48 +0800

    fix：修复点击遮罩，无法再打开弹窗

--- src/NativeDatePickerView.ts
+++ src/NativeDatePickerView.ts
@@ -1,26 +1,27 @@
-import type { ViewProps } from "react-native/Libraries/Components/View/ViewPropTypes";
-import type { HostComponent } from "react-native";
-import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
+import type { ViewProps } from "react-native/Libraries/Components/View/ViewPropTypes"; 
+import type { HostComponent } from "react-native"; 
+import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent"; 
 import type {
     Float,
     WithDefault,
     BubblingEventHandler,
     Double,
-  } from 'react-native/Libraries/Types/CodegenTypes';
-  type DateEvent =  Readonly<{
-    timestamp: string
-  }>
-export interface NativeProps extends ViewProps {
-    date?: string,
-    modal?: boolean,
-    open?:boolean,
-    mode?: WithDefault<'date' | 'time' | 'datetime', 'datetime'>,
-    onChange?: BubblingEventHandler<DateEvent>,
-    onConfirm?: BubblingEventHandler<DateEvent>,
-    onCancel?: BubblingEventHandler<undefined>,
-    onDateChange?: BubblingEventHandler<DateEvent>,
-    maximumDate?: string,
+  } from 'react-native/Libraries/Types/CodegenTypes'
+  type DateEven  = Readonly<{
+    timestamp:string
+  }
+  >
+export interface NativeProps extends ViewProps {   
+    mode?: WithDefault<'date' | 'datetime' | 'time','date' >;
+    date?: string
+    open?: boolean
+    modal?:boolean
+    onConfirm?:BubblingEventHandler<DateEven>;
+    onDateChange?:BubblingEventHandler<DateEven>;
+    onChange?:BubblingEventHandler<DateEven>;
+    onCancel?:BubblingEventHandler<undefined>;
+    maximumDate?: string
     minimumDate?: string
- // 添加其它 props
-}
-export default codegenNativeComponent<NativeProps>("NativeDatePickerView") as HostComponent<NativeProps>;
+} 
+ 
+export default codegenNativeComponent<NativeProps>("NativeDatePickerView") as HostComponent<NativeProps>; 
